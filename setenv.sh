b7538a247ea6c7bf97cd0d3e4de507cf98f8dcdd
Igor Babuschkin <igor@babuschk.in>
2014-07-18 17:35:12 +0200

    Move project to public afs

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,4 @@
-export User_release_area=/afs/cern.ch/user/i/ibabusch/Code/EventMerge/cmtuser
+export PS1="*$PS1"
+export User_release_area=/afs/cern.ch/user/i/ibabusch/public/MergeEvent/cmtuser
 . SetupProject.sh DaVinci v35r1
 
