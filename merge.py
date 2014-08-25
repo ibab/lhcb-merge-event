6b9de3cae89846096750d3f6832e750a39803838
Igor Babuschkin <igor@babuschk.in>
2014-08-25 11:58:31 +0200

    Replicate stripping cuts for D_s+

--- merge.py
+++ merge.py
@@ -12,12 +12,43 @@ from OtherMCParticles import *
 GaudiPersistency()
 importOptions("data_local.py")
 
+ds_mass = 1968.47
+
+combo_cut = [ 'ASUM(PT) > 1800*MeV' # ASUMPT
+            , "AMAXDOCA('LoKi::DistanceCalculator') < 0.5*mm" # AMAXDOCA
+            ]
+mom_cut = [ 'VFASPF(VCHI2/VDOF) < 10' # VCHI2DOF
+          , 'in_range(%s, MM, %s)' % (str(ds_mass-100)+'*MeV', str(ds_mass+100)+'*MeV')
+          ]
+
+daug_cut = [ 'TRCHI2DOF < 3'
+           , 'PT > 100*MeV'
+           , 'P > 100*MeV'
+           , 'MIPCHI2DV(PRIMARY) > 4' # MIPCHI2DV
+           , 'TRGHP < 0.4'
+           ]
+
+combo_cut = ' & '.join(map(lambda x: '(' + x + ')', combo_cut))
+mom_cut = ' & '.join(map(lambda x: '(' + x + ')', mom_cut))
+daug_cut = ' & '.join(map(lambda x: '(' + x + ')', daug_cut))
+
+print("CombinationCut = " + combo_cut)
+print("MotherCut = " + mom_cut)
+print("DaughtersCut = " + mom_cut)
+
 # Selection for other loop
 _otherKaons = DataOnDemand(Location='/Event/NewEvent/Phys/OtherAllKaons/Particles')
 _otherPions = DataOnDemand(Location='/Event/NewEvent/Phys/OtherAllPions/Particles')
 _otherd2kkpi = CombineParticles("otherd2kkpi")
 _otherd2kkpi.DecayDescriptor = "D_s- -> K- K+ pi-"
-_otherd2kkpi.MotherCut = "(mcMatch('[D_s+  ==> K- K+ pi+]CC'))"
+#_otherd2kkpi.MotherCut = "(mcMatch('[D_s+  ==> K- K+ pi+]CC'))"
+_otherd2kkpi.CombinationCut = combo_cut
+_otherd2kkpi.MotherCut = mom_cut
+_otherd2kkpi.DaughtersCuts = { "K-": daug_cut,
+                               "K+": daug_cut,
+                               "pi-": daug_cut,
+                               "pi+": daug_cut,
+                             }
 _otherd2kkpi.Preambulo = [
     "from LoKiPhysMC.decorators import *",
     "from PartProp.Nodes import CC" ]
@@ -38,12 +69,12 @@ evtAlgs = GaudiSequencer("EventAlgs",
                          Members=[RegisterAddr(AddressesFile='eventaddr.txt', OutputLevel=DEBUG),
                                   #MergeEvent(),
                                   makeparts,
+                                  expl,
                                   seqD2KKPiOther.sequence(),
-                                  expl
                                   ])
 
 from Configurables import DaVinci
-DaVinci().EvtMax = 1
+DaVinci().EvtMax = 100
 DaVinci().PrintFreq = 1
 DaVinci().SkipEvents = 0
 DaVinci().DataType = "2011"
